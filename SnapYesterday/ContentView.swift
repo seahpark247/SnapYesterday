7ae51c3286044f82c803d96e87174abe3d0399ef
seahpark247 <famousjedo@gmail.com>
2025-05-20 07:51:37 -0500

    feat: SnapYesterday features

--- SnapYesterday/ContentView.swift
+++ SnapYesterday/ContentView.swift
@@ -8,14 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var today = ""
+    @State private var yesterday = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        NavigationStack {
+            Form {
+                Section("How do I feel right now") {
+                    TextField("Enter your feelings", text: $today)
+                    
+                    HStack {
+                        Spacer()
+                        Button("Save") {
+                            UserDefaults.standard.set(today, forKey: "savedSnap")
+                        }
+                    }
+                }
+                
+                Section("Check snap from yesterday!") {
+                    HStack {
+                        Spacer()
+                        Button("Snap Yesterday") {
+                            yesterday = UserDefaults.standard.string(forKey: "savedSnap") ?? "No snap from yesterday!"
+                        }
+                    }
+                    Text(yesterday)
+                }
+            }
+            .navigationTitle("Snap Yesterday")
+            .background(.purple.gradient)
+        }.scrollContentBackground(.hidden)
     }
 }
 
